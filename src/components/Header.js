c45fcceebf94d0fcde75472045c63837098e9d85
sankar-tn-03 <159781939+sankar-tn-03@users.noreply.github.com>
2024-08-07 12:22:23 +0530

    new changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import { FaUserCircle } from "react-icons/fa";
+import '../styles/header.css';
+import { SiIrobot } from "react-icons/si";
 
-
-function Home(){
+function Header(){
     return(
         <>
-        <div>
-            Header Section
+        <div className="header-navbar">
+           <div className="logo-container">
+           <SiIrobot />
+           </div>
+           <div className="profile-container">
+                <FaUserCircle />
+                <span>Sankar</span>
+           </div>
         </div>
         </>
     );
 }
 
-export default Home;
+export default Header;
